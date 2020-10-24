c18f3cb5a8d8ba12ba04396bdb85a63e0556db8d
Nick <38957420+nickh671@users.noreply.github.com>
2020-10-24 13:57:33 -0400

    q7 kinda done

--- q7.py
+++ q7.py
@@ -1,6 +1,27 @@
 def main(str1):
-    return "return"
+    openbracket = 0
+    closebracket = 0
+    lastOpen = False
+    balance = False
+    illegal = False
 
+    for char in str1:
+        if char not in ['[', ']']:
+            illegal = True
+            break
+        else:
+            if char == '[':
+                openbracket += 1 
+            if char == ']':
+                closebracket += 1
+
+    if illegal:
+        return 'Invalid character.'
+    elif closebracket == openbracket:
+        return 'Balanced.'
+    else:
+        return 'Not Balanced.'
+        
 if __name__ == "__main__":
     print(
         main(
