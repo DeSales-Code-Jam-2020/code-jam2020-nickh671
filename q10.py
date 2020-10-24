a910ea88651b688da0507738d3ebddf25b6563b9
Nick <38957420+nickh671@users.noreply.github.com>
2020-10-24 13:01:46 -0400

    q10 done

--- q10.py
+++ q10.py
@@ -1,5 +1,17 @@
 def main(str1, str2, int1):
-    return "return"
+    for i in range(len(str1)):
+        if str1[i] == 'A':
+            if str2[i].isdigit():
+                return "Invalid plate number"
+        elif str1[1] == 'N':
+            if str2[i].isalpha():
+                return "Invalid plate number"
+
+    last = str2[len(str2)-1]
+
+    result = str2[:-1]
+    result += chr(ord(last) + int1)
+    return result
 
 if __name__ == "__main__":
     print(
