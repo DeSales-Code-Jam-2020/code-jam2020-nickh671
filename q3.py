08696a50560d09aa07516173e8cd5b54ff03f760
Nick <38957420+nickh671@users.noreply.github.com>
2020-10-24 12:13:29 -0400

    q3 done

--- q3.py
+++ q3.py
@@ -1,5 +1,12 @@
+import math
+
 def main(int1, int2):
-   return "Your return here"
+    result = ''
+    gcd = math.gcd(int1, int2)
+    top = int1 / gcd
+    bottom = int2 / gcd
+    result = '{:.0f}/{:.0f}'.format(top, bottom)
+    return result
 
 if __name__ == "__main__":
     print(
