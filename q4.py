bfdf301915ab626e1cb20c5f38a4d982e2a5c667
Nick <38957420+nickh671@users.noreply.github.com>
2020-10-24 12:26:02 -0400

    q4 done

--- q4.py
+++ q4.py
@@ -1,5 +1,28 @@
 def main(str1):
-    return "return"
+    result = ''
+    shift = ''
+    start = 0
+    for i in range(len(str1)):
+        if str1[i] not in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+            shift += str1[i]
+        else:
+            start += i
+            break
+
+    if start == 0:
+        result += str1
+        result += 'ay'
+        return result
+    else:
+
+        while start < len(str1):
+            result += str1[start]
+            start += 1
+
+
+        result += shift
+        result += 'ay'
+        return result
 
 if __name__ == "__main__":
     print(
