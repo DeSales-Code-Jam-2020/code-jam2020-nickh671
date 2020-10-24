8d1a5358604b726a8c42de6bc0356574b6425613
Nick <38957420+nickh671@users.noreply.github.com>
2020-10-24 13:47:36 -0400

    q11 done

--- q11.py
+++ q11.py
@@ -1,5 +1,18 @@
 def main(str1, str2):
-    return str1.count(str2) + str1[::-1].count(str2)
+    same = True
+    first = str1[0]
+    for char in str1:
+        if str1[1] != first:
+            same = False
+    
+    if same == False:
+        reverse = str1[::-1]
+        count = str1.count(str2)
+        count2 = reverse.count(str2)
+        return count + count2
+    else:
+        count = len(str1) + len(str2) - 1
+        return count
 
 if __name__ == "__main__":
     print(
