0f3704982d842c3739e945b2d1f70cd3f454208d
Nick <38957420+nickh671@users.noreply.github.com>
2020-10-24 12:48:04 -0400

    q9 done

--- q9.py
+++ q9.py
@@ -1,5 +1,13 @@
+import collections
+
 def main(str1, str2):
-    return "return"
+    word1 = collections.Counter(str1)
+    word2 = collections.Counter(str2)
+
+    if word1 == word2:
+        return "The strings are anagrams."
+    else:
+        return "The strings are not anagrams."
 
 if __name__ == "__main__":
     print(
