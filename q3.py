26bfe7ecfb1549661c02c16b368303ab592550db
Nick <38957420+nickh671@users.noreply.github.com>
2020-10-24 13:12:06 -0400

    q3 fix

--- q3.py
+++ q3.py
@@ -5,7 +5,7 @@ def main(int1, int2):
     gcd = math.gcd(int1, int2)
     top = int1 / gcd
     bottom = int2 / gcd
-    result = '{:.0f}/{:.0f}'.format(top, bottom)
+    result = 'The reduced fraction is: {:.0f}/{:.0f}'.format(top, bottom)
     return result
 
 if __name__ == "__main__":
